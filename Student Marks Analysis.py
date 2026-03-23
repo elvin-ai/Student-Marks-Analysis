0818bbca5f6aed851d67d9faaaa29645f1cd688c
Elvin Pillay <elvinpillay1702@gmail.com>
2026-03-23 16:50:59 +0530

    Add files via upload

---
+++ Student Marks Analysis.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+print("Hello. Welcome to the student mark analyser project created by Elvin Pillay.")
+names=input("Enter names : \n").split()
+marks=list(map(int,input("Enter marks : \n").split()))
+
+m=np.array(marks)
+n=np.array(names)
+
+avg=np.average(m)
+mean=np.mean(m)
+m1=np.max(m)
+hm=np.argmax(m)
+topper=n[hm]
+
+print("Highest Marks  : ", m1)
+print("Highest Scorer : ",topper)
+
+plt.bar(n,m)
+plt.title("Analysis")
+plt.xlabel("Students")
+plt.ylabel("Marks")
+plt.show()
